70240cd9aafd627922d7a1af5198588e61264056
Davide Mauri <damauri@microsoft.com>
2024-11-10 12:33:50 -0800

    moved to vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
 import SearchApp from './SearchApp';
-import './SearchApp.styles'; // Import the styles to apply global styles
-
-// Dynamically load the Roboto font
-const fontLink = document.createElement('link');
-fontLink.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap';
-fontLink.rel = 'stylesheet';
-document.head.appendChild(fontLink);
+import './assets/styles/global.css'; // Global styles (optional)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
