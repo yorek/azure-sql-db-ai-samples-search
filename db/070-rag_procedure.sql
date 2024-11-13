247af416606777d2aa63e72130085dbbc8e69861
Davide Mauri <damauri@microsoft.com>
2024-11-13 10:13:28 -0800

    improved search

--- db/070-rag_procedure.sql
+++ db/070-rag_procedure.sql
@@ -25,7 +25,14 @@ order by
     distance_score asc
 --select * from #s
 
-declare @s nvarchar(max) = (select [id], [name], [description], [notes], [details] from #s for json path)
+declare @s nvarchar(max) = (
+    select 
+        [id], [name], [description], [notes], [details], 
+        cast((1-distance_score)*100 as int) as similiarity_score
+    from #s 
+    where distance_score < 0.5 
+    order by distance_score for json path
+)
 --select @s
 
 declare @p nvarchar(max) = 
@@ -35,9 +42,10 @@ json_object(
             'role':'system',
             'content':'
                 You as a system assistant who helps users find code samples the user can use to learn the topic they are interested in.
-                Samples are provided in an assitant message using a JSON Array with the following format: [{id, name, description, note, details}]. 
+                Samples are provided in an assitant message using a JSON Array with the following format: [{id, name, description, note, details, similiarity_score}]. 
                 Use the provided samples to help you answer the user''s question.
-                Put int sample_summary output property a short summary of the sample using the provided description, notes, and details.
+                Put in sample_summary output property a markdown short summary of the sample using the provided description, notes, and details. 
+                If there are related links or repos in the details, include them in the short summary.                
                 If the question is not answered by the samples, you can say that you don''t know.
                 If users ask about topics you don''t know, answer that you don''t know.
                 Return up to 10 samples if you can.
@@ -45,7 +53,11 @@ json_object(
         ),
         json_object(
             'role':'assistant',
-            'content': 'The available samples are: ' + @s              
+            'content': 'The available samples are the following:'
+            ),
+        json_object(
+            'role':'assistant',
+            'content': @s
             ),
         json_object(
             'role':'user',
