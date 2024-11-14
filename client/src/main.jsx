fd974429c5ce95207af821e7214df2196c6b1fcd
Davide Mauri <damauri@microsoft.com>
2024-11-14 12:54:39 -0800

    using web light theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import SearchApp from './SearchApp';
 import './assets/styles/global.css'; // Global styles (optional)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <FluentProvider theme={teamsLightTheme}>
+  <FluentProvider theme={webLightTheme}>
     <SearchApp />
   </FluentProvider>
 );
