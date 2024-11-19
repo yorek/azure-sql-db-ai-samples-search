be4c504d0cff2e35afbc9a0197e20236f082ff66
Davide Mauri <damauri@microsoft.com>
2024-11-19 15:56:44 -0500

    fixed paths

--- db/Program.cs
+++ db/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using DbUp;
 using DbUp.ScriptProviders;
@@ -10,10 +11,13 @@ namespace Database.Deploy
 {
     class Program
     {
+        const string sqlFolder = "./db/sql";
+        const string envFile = ".env";
+
         static int Main(string[] args)
         {
             // This will load the content of .env and create related environment variables
-            DotNetEnv.Env.Load("../.env");            
+            Env.Load(envFile);           
 
             // Connection string for deploying the database (high-privileged account as it needs to be able to CREATE/ALTER/DROP)
             var connectionString = Env.GetString("MSSQL");
@@ -33,6 +37,7 @@ namespace Database.Deploy
             conn.Close();
 
             FileSystemScriptOptions options = new() {
+                
                 IncludeSubDirectories = false,
                 Extensions = ["*.sql"],
                 Filter = (f) => !f.Contains(".local."),
@@ -42,14 +47,15 @@ namespace Database.Deploy
             Dictionary<string, string> variables = new() {
                 {"OPENAI_URL", Env.GetString("OPENAI_URL")},
                 {"OPENAI_KEY", Env.GetString("OPENAI_KEY")},
-                {"OPENAI_MODEL", Env.GetString("OPENAI_MODEL")}
+                {"OPENAI_EMBEDDING_DEPLOYMENT_NAME", Env.GetString("OPENAI_EMBEDDING_DEPLOYMENT_NAME")},
+                {"OPENAI_CHAT_DEPLOYMENT_NAME", Env.GetString("OPENAI_CHAT_DEPLOYMENT_NAME")}
             };
 
             Console.WriteLine("Starting deployment...");
             var dbup = DeployChanges.To
                 .SqlDatabase(csb.ConnectionString)
                 .WithVariables(variables)
-                .WithScriptsFromFileSystem("sql", options)
+                .WithScriptsFromFileSystem(sqlFolder, options)
                 .JournalToSqlTable("dbo", "$__dbup_journal")                                               
                 .LogToConsole()
                 .Build();
