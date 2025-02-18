990fa3a95b6370c930a035e52ede6cb54329395d
Davide Mauri <damauri@microsoft.com>
2025-02-18 11:39:55 -0800

    improved prompt engineering

--- db/sql/11-orchestrate_request.sql
+++ db/sql/11-orchestrate_request.sql
@@ -33,12 +33,12 @@ json_object(
                     [updated_on] datetime2(0) not null
                 )
                 
-                To search into "details, "notes" and "description" columns, you can use the following steps:
+                Any time search into "details, "notes" and "description" columns is needed, you must use the following steps:
 
-                First, generate the embedding vector for the provided question using the following T-SQL query. 
+                First, generate the embedding vector for the provided question using the following T-SQL query. ''<search text'' must be generating taking the relevant part from the user question.
 
                 declare @retval int, @qv vector(1536)
-                exec @retval = web.get_embedding ''<user_question>'', @qv output
+                exec @retval = web.get_embedding ''<search text>'', @qv output
                 if (@retval != 0) throw 50000, ''Error in getting the embedding'',1;
 
                 The vectors for details, notes and description columns are stored in the following tables: 
@@ -65,18 +65,23 @@ json_object(
                 left join
                     dbo.samples_details_embeddings de on e.id = de.id    
                 order by 
-                    distance_score asc;
+                    distance_score asc
 
-                always return the distance_score. 
+                When search in description, details and notes columns is not needed then you must use the following query:
 
-                To use the LIKE operator on details column, the column must be converted to NVARCHAR(MAX) first:
+                select top(@k) 
+                    s.id, [name], [description], [notes], [details],
+                    0.0 as distance_score
+                from 
+                    dbo.samples s
+
+                In any case to use the LIKE operator on details column, the column must be converted to NVARCHAR(MAX) first:
 
                 CAST([details] AS NVARCHAR(MAX)) LIKE ''search text''
 
-                Return the top 50 results maximum.                 
-                Always return only the following columns from the generated query: id, name, description, notes, details, distance_score    
                 The use question is provided in the next message. If the user question cannot be answered using the dbo.samples table and using a T-SQL query only, you should respond with an empty string.
                 Return the top 10 results if you can. Do not use semicolon to terminate the T-SQL statement.                
+                Only return the following columns: id int, [name] nvarchar(100), [description] nvarchar(max), notes nvarchar(max), details json, distance_score float.
                 You can generate only SELECT statements. If the user is asking something that will generate INSERT, UPDATE, DELETE, CREATE, ALTER or DROP statement, refuse to generate the query.
 
             '
--- db/utils-scripts/hybrid-search-with-prompt-engineering.sql
+++ db/utils-scripts/hybrid-search-with-prompt-engineering.sql
@@ -10,6 +10,7 @@ declare @text nvarchar(max);
 --set @text = 'create a new table named dbo.test';
 --set @text = 'how many customers there are in the customers table?';
 set @text = 'Find all the hybrid search samples created after 2025';
+--set @text = 'Show me the latest samples'
 
 declare @retval int, @response nvarchar(max);
 
@@ -38,7 +39,7 @@ json_object(
                     [updated_on] datetime2(0) not null
                 )
                 
-                To search into "details, "notes" and "description" columns, you can use the following steps:
+                Any time search into "details, "notes" and "description" columns is needed, you must use the following steps:
 
                 First, generate the embedding vector for the provided question using the following T-SQL query. ''<search text'' must be generating taking the relevant part from the user question.
 
@@ -70,17 +71,23 @@ json_object(
                 left join
                     dbo.samples_details_embeddings de on e.id = de.id    
                 order by 
-                    distance_score asc;
+                    distance_score asc
 
-                always return the distance_score. 
+                When search in description, details and notes columns is not needed then you must use the following query:
 
-                To use the LIKE operator on details column, the column must be converted to NVARCHAR(MAX) first:
+                select top(@k) 
+                    s.id, [name], [description], [notes], [details],
+                    0.0 as distance_score
+                from 
+                    dbo.samples s
+
+                In any case to use the LIKE operator on details column, the column must be converted to NVARCHAR(MAX) first:
 
                 CAST([details] AS NVARCHAR(MAX)) LIKE ''search text''
 
-                Return the top 50 results maximum.                 
                 The use question is provided in the next message. If the user question cannot be answered using the dbo.samples table and using a T-SQL query only, you should respond with an empty string.
                 Return the top 10 results if you can. Do not use semicolon to terminate the T-SQL statement.                
+                Only return the following columns: id int, [name] nvarchar(100), [description] nvarchar(max), notes nvarchar(max), details json, distance_score float.
                 You can generate only SELECT statements. If the user is asking something that will generate INSERT, UPDATE, DELETE, CREATE, ALTER or DROP statement, refuse to generate the query.
 
             '
