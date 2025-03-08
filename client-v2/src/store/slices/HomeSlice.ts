2966422813756f1d44326e557fbc8aed92d4e1e2
Raffaele Garofalo <raffaeu@gmail.com>
2025-03-08 18:14:02 +0100

    feat: update HomeSlice API call and enhance CORS settings; configure HTTPS for local development

--- client-v2/src/store/slices/HomeSlice.ts
+++ client-v2/src/store/slices/HomeSlice.ts
@@ -4,8 +4,14 @@ import axios from 'axios';
 
 // async search total samples
 export const getTotalSamplesAsync = createAsyncThunk<number>('home/getTotalSamples', async () => {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}countSamples`);   
-    return response.data?.value[0]?.totalSamples;
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}countSamples`, {
+      withCredentials: false,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    });   
+    return response.data?.value[0]?.total_sample_count;
 });
 
 const initialState: HomeState = {
@@ -30,6 +36,7 @@ const HomeSlice = createSlice({
           .addCase(getTotalSamplesAsync.rejected, (state, action) => {
             state.status = 'failed';
             state.error = action.error.message;
+            console.error(action.error);
           });
     }
 });
