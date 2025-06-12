a34850abd0765c7e24a126bfcb7ee89d15379958
Davide Mauri <damauri@microsoft.com>
2025-06-12 08:52:52 -0700

    added build recording

---
+++ db/samples/build-2025.sql
@@ -0,0 +1,18 @@
+/*
+    SQL Conf() 2025
+*/
+
+exec dbo.add_sample '
+{
+    "name": "Build Agentic RAG solutions with Azure SQL OpenAI & Data API builder | DEM563", 
+    "description": "Join us for an exciting demonstration on how to transform raw data in a database into a searchable format using Natural Language Processing (NLP). You will learn how to leverage Retrieval-Augmented Generation (RAG), vectors, NL2SQL, and agents, all within T-SQL, to create a powerful and scalable solution. Don''t miss this opportunity to see how you can build a full-stack end-to-end solution in just 15 minutes!",
+    "url": "https://www.youtube.com/watch?v=suqgkUZ9TgU",
+    "details": {
+        "type": "recording",
+        "author": "Davide Mauri",        
+        "conference": "Microsoft Build 2025"     
+    }
+}
+';
+GO
+
