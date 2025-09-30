abfc1ddb9847884049f2b39a197f2b9d5245391f
Davide Mauri <damauri@microsoft.com>
2025-09-30 09:11:24 -0700

    improved vertical placement

--- client-v2/src/components/messages/HowItWorks.style.ts
+++ client-v2/src/components/messages/HowItWorks.style.ts
@@ -3,7 +3,8 @@ import { makeStyles } from "@fluentui/react-components";
 const Styles = makeStyles({
     dialogSurface: {
         width: '50vw',
-        maxWidth: '50vw'
+        maxWidth: '50vw',
+        top: '-25vw'
     }
 });
 
